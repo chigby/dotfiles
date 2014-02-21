0412f2fd3e969cacc3729e90f07e66fbf1e7e0e2
Cameron Higby-Naquin <cameron.higbynaquin@gmail.com>
2014-02-21 13:12:22 -0500

    Offlineimap to mbsync.

--- bin/get_password.py
+++ bin/get_password.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python2.7
+
 import os
+import sys
 
 def get_password(account):
     cmd = "/usr/bin/security 2>&1 >/dev/null find-generic-password -a {0} -g".format(account)
@@ -8,3 +11,10 @@ def get_password(account):
         return passwds[-1][1:-1].decode('string_escape')
     else:
         return ''
+
+def main(argv):
+    print(get_password(argv[1]))
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
