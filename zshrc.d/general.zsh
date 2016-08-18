5713a9a93186f87a088bf4832a0788aa4237037f
Cameron Higby-Naquin <me@chigby.org>
2016-08-18 09:38:09 -0400

    More informative comment.

--- zshrc.d/general.zsh
+++ zshrc.d/general.zsh
@@ -2,5 +2,12 @@
 # see https://github.com/robbyrussell/oh-my-zsh/issues/449
 unsetopt nomatch
 
-# higher util word characters
-export WORDCHARS='*?[]~&;!$%^<>'
+# modify word chars to exclude certain characters to easier editing
+# with backward-word-kill and similar commands:
+# "/" so we don't remove entire paths at once
+# "." for finer-grained address/extension manipulation
+# "_" and "-" for delimited words within filenames
+# "(){}" for shell scripts in-line
+# "=" for defining environment variable values
+# "~" since it appears in many paths
+export WORDCHARS='*?[]~&;!#$%^<>'
