383768167452c9bf3218270b3dc810bb408c46b6
Cameron Higby-Naquin <me@chigby.org>
2016-09-06 17:58:10 -0400

    Very useful option.

--- zshrc.d/history.zsh
+++ zshrc.d/history.zsh
@@ -1,3 +1,7 @@
 HISTFILE=$HOME/.zhistory
 HISTSIZE=100000
 SAVEHIST=100000
+
+# Do not enter command lines into the history list if they are
+# duplicates of the previous event.
+setopt histignoredups
