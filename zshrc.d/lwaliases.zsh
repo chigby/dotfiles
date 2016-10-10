1c8ed661c2ec27c6145f4e284886aa58b0d6e0e7
Cameron Higby-Naquin <me@chigby.org>
2016-10-10 14:14:09 -0700

    Shortcut for lcv.

--- zshrc.d/lwaliases.zsh
+++ zshrc.d/lwaliases.zsh
@@ -1,5 +1,6 @@
 alias br="cd $HOME/lw/django-brambling && workon brambling"
 alias bf="cd $HOME/lw/mybfinfo && workon mybfinfo"
+alias lcv="cd $HOME/lw/lcv && workon lcv"
 
 function mr() {
   cd ~/Downloads/elasticsearch-1.7.5 && ./bin/elasticsearch -d
