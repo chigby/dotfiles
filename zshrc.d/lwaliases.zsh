53e5918fb5c6f65e7e21d5fc11b2a4a5819baa43
Cameron Higby-Naquin <me@chigby.org>
2016-12-19 17:34:12 -0500

    Helper for lcv's npm webpack stuff

--- zshrc.d/lwaliases.zsh
+++ zshrc.d/lwaliases.zsh
@@ -1,6 +1,7 @@
 alias br="cd $HOME/lw/django-brambling && workon brambling"
 alias bf="cd $HOME/lw/mybfinfo && workon mybfinfo"
 alias lc="cd $HOME/lw/lcv && workon lcv"
+alias lcr="cd $HOME/lw/lcv/lcvista/react && npm run start"
 
 function mr() {
   cd ~/Downloads/elasticsearch-1.7.5 && ./bin/elasticsearch -d
